87894ef1cdc7857d4db23d6fb4c7614a931d6cef
jonnathan77 <jonnathanfsouzacruz@gmail.com>
2023-03-13 21:20:51 -0300

    feat

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,17 @@
 import { Component, OnInit } from '@angular/core';
-
+// import  { players } from '../../../src/app/players'
 @Component({
   selector: 'app-home',
   templateUrl: './home.page.html',
   styleUrls: ['./home.page.scss'],
+  providers: [
+    //other providers ...
+    players,
+  ],
 })
 export class HomePage implements OnInit {
 
+  
   slideOpts = {
     initialSlide: 1,
     speed: 400
